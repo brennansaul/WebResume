9c3ba7076b70a6248c6597337b656635adc75c12
Brennan Saul <jamessaul7629@gmail.com>
2017-06-27 11:40:13 -0500

    file restructure

---
+++ saulWeb/public/src/app/experiences/experiences.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'experiences',
+  templateUrl: './experiences.component.html',
+  styleUrls: ['./experiences.component.css']
+})
+export class ExperiencesComponent {
+
+}
