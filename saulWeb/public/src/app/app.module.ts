1f13e30b34f53098dada00ff88e2a535aa634472
Brennan Saul <jamessaul7629@gmail.com>
2017-07-07 18:08:43 -0500

    Added routing and a header component that contains the navbar

--- saulWeb/public/src/app/app.module.ts
+++ saulWeb/public/src/app/app.module.ts
@@ -1,20 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { SkillsComponent} from './skills/skills-display.component';
 import { ExperiencesComponent } from './experiences/experiences.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { HeaderComponent } from './header/header.component';
+
+const appRoutes: Routes = [
+  { path: '', component: WelcomeComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'experiences', component: ExperiencesComponent }
+  //{ path: 'projects', component: ProjectComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     SkillsComponent,
     ExperiencesComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ saulWeb/public/src/app/header/header.component.ts
@@ -0,0 +1,23 @@
+/*
+* File:        header.component.ts
+* Author:      Brennan Saul
+* Description: Component for the navbar and router-outlet
+*
+* Edit history:
+*
+* Editor   Date		  Description
+* ======   ========	===========
+* Saul     7/7/17   File Created
+*/
+
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+
+export class HeaderComponent {
+
+}
